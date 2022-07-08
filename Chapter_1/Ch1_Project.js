bcf5b2d0173edb189526c64992cde83f06ad3cb6
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2022-07-08 15:34:40 -0400

    First Commit

---
+++ Chapter_1/Ch1_Project.js
@@ -0,0 +1,9 @@
+
+/* This Javascript file contains a variable to store my name 
+and outputs the stored name to the console */
+
+// Variable to store name
+let name = "Chris Vatos";
+
+// Prints the variable name to the console
+console.log(name);
---
+++ Chapter_1/app.js
@@ -0,0 +1,24 @@
+
+console.log("Hi there");
+
+// Setting the variable a to contain a value of 10
+let a  = 10;
+
+// Pringting to the console the value a stores
+console.log(a);
+
+/* This is the syntax for a multiple line comment in Javascript. The opening is a forward slash followed by an 
+asterisk and the closing is an asterisk follwoed by a forward slash.
+*/
+
+// Code to generate a random number between 0 and 1
+Math.random();
+
+// Prints to the console a number between 0 and 1
+console.log(Math.random());
+
+// Prints to the console a number between 0 and 100
+console.log(Math.random() * 100);
+
+// Prints to the console a whole number between 0 and 100
+console.log(Math.floor(Math.random() * 100));
