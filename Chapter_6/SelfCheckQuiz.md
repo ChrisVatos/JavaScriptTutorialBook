496421468edf06e2edbe1cc3e1f9fb4c03060759
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2022-07-14 16:58:30 -0400

    Finishing Chapter 6

---
+++ Chapter_6/SelfCheckQuiz.md
@@ -0,0 +1,14 @@
+1) 10   (since val is global and were logging it outside; not setting the return value of the function to a variable)
+
+
+2) Hello
+
+3)  Welcome
+    Laurence
+    My name is Laurence
+
+
+4) 19
+
+
+5) 16
---
+++ Chapter_6/chapter_projects.js
@@ -0,0 +1,33 @@
+// Create a recursive function
+
+function countToTen(nr) {
+    if(nr > 10) {
+        return;
+    } else {
+        console.log(nr);
+        countToTen(++nr);
+    }
+}
+countToTen(6);
+
+
+// Set timeout order
+
+let outputOne = () => console.log("One");
+let outputTwo = () => console.log("Two");
+
+let outputThree = () => {
+    console.log("Three");
+    outputOne();
+    outputTwo();
+}
+
+let outputFour = () => {
+    console.log("Four");
+    setTimeout(outputOne, 0);
+    outputThree();
+}
+
+outputFour();
+
+
