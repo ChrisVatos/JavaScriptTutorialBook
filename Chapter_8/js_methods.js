0cac34b7f98813b91fab210164d3b0d7032d7ad2
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2022-07-18 12:01:28 -0400

    Starting Chapter 8

---
+++ Chapter_8/js_methods.js
@@ -0,0 +1,5 @@
+/* Built-In JavaScript Methods --> Functionality that we get out of the box with JS
+                               --> Use methods that we haven't coded ourselves
+                               --> Difference between a function and a method is that a function is defined anywhere within
+                                   the script whereas a method is defined within a class
+                               --> Methods are essentially functions on classes and instances  */
