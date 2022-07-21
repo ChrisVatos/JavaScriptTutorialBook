7dab4529480a67a498bf0d7492f83049491aa178
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2022-07-21 11:21:43 -0400

    Beginning Number Methods

---
+++ Chapter_8/number_methods.js
@@ -0,0 +1,7 @@
+/* isNan() --> Returns true if a value is "not a number" and false otherwise
+           --> Possible to negate the function so it returns true if a value is a number    */
+
+let x = 34;
+console.log(isNaN(x));      // false
+console.log(!isNaN(x));     // true
+let str = "hi";
